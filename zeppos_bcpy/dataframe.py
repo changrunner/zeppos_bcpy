77bc5ae1853cb27c1da5325ee419a27a7856a1c5
Haegdorens, Gert <gert.haegdorens@yrcw.com>
2020-11-26 05:01:00 -0600

    add csv_file_name to the columns

--- zeppos_bcpy/dataframe.py
+++ zeppos_bcpy/dataframe.py
@@ -5,6 +5,7 @@ from zeppos_bcpy.bcp import Bcp
 from zeppos_data_manager.df_cleaner import DfCleaner
 from zeppos_bcpy.sql_table import SqlTable
 from datetime import datetime
+from os import path
 
 class Dataframe:
     def __init__(self, sql_configuration):
@@ -20,17 +21,19 @@ class Dataframe:
     @staticmethod
     def to_sqlserver_creating_instance(
             pandas_dataframe, sql_configuration, index=False, discover_data_type=False, use_existing=False,
-            batch_size=10000, audit_date=datetime.utcnow()):
+            batch_size=10000, audit_date=datetime.utcnow(), csv_full_file_name=None):
         dataframe = Dataframe(sql_configuration)
-        dataframe.to_sqlserver(pandas_dataframe, index, discover_data_type, use_existing, batch_size, audit_date)
+        dataframe.to_sqlserver(pandas_dataframe, index, discover_data_type, use_existing, batch_size,
+                               audit_date, csv_full_file_name)
         return dataframe
 
     def to_sqlserver(self, pandas_dataframe, index=False, discover_data_type=False,
-                     use_existing=False, batch_size=10000, audit_date=datetime.utcnow()):
+                     use_existing=False, batch_size=10000, audit_date=datetime.utcnow(),
+                     csv_full_file_name=None):
         # set properties first
         self.pandas_dataframe = \
             Dataframe._conform_pandas_dataframe(
-                Dataframe._add_audit_fields(pandas_dataframe, audit_date)
+                Dataframe._add_audit_fields(pandas_dataframe, audit_date, csv_full_file_name)
             )
         self.index = index
         self.discover_data_type = discover_data_type
@@ -52,6 +55,8 @@ class Dataframe:
         return pandas_dataframe
 
     @staticmethod
-    def _add_audit_fields(pandas_dataframe, audit_date):
+    def _add_audit_fields(pandas_dataframe, audit_date, csv_full_file_name):
         pandas_dataframe['AUDIT_CREATE_UTC_DATETIME'] = audit_date
+        if csv_full_file_name:
+            pandas_dataframe['CSV_FILE_NAME'] = path.basename(csv_full_file_name)
         return pandas_dataframe
